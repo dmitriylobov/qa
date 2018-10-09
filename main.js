f291966f1d067d9cd0c648f17b1eab5282118823
DmitriyLobov <dimkalobov95@gmail.com>
2018-10-09 16:27:34 +0300

    slim-scroll

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 import style from './styles/main.scss';
-document.onload = function () {
+import * as $ from 'jquery';
+
+$(document).ready(function () {
     $('.carousel').carousel();
     style.use();
+});
 
-};
+// slim scroll
+$(document).ready(function () {
+    $("a.scrollLink").click(function (event) {
+        event.preventDefault();
+        $("html, body").animate({scrollTop: $($(this).attr("href")).offset().top}, 750);
+    });
+});
