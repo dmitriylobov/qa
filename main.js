97a655aaac7cdbc650f216141706c90c7eb1ec7e
dimkalobov95 <dimkalobov95@gmail.com>
2018-10-11 20:48:32 +0300

    markup completed

--- main.js
+++ main.js
@@ -6,9 +6,26 @@ $(document).ready(function () {
         event.preventDefault();
         $("html, body").animate({scrollTop: $($(this).attr("href")).offset().top}, 750);
     });
+
+    $("#contactForm").submit(submitForm);
+
+    function submitForm($event) {
+        $event.preventDefault();
+        const controls = {
+            name: $('.form-group input[name="name"]'),
+            lastName: $('.form-group input[name="lastname"]'),
+            email: $('.form-group input[name="email"]'),
+            phone: $('.form-group input[name="phone"]'),
+            comment: $('.form-group textarea[name="comment"]')
+        };
+
+        Object.entries(controls).forEach(([key, value]) => console.log(`${key}=${value.val()}`));
+        Object.entries(controls).forEach(([key, value]) => value.val(''));
+    }
 });
 
 document.ready = function () {
     $('.carousel').carousel();
     style.use();
 };
+
